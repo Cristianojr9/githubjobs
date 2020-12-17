a10a8e769a9f59cdca080cef31e46616cfb99a93
cristianojr9 <cristiano_junior99@outlook.com>
2020-12-17 17:57:39 -0300

    Implementacao da listagem dos jobs

--- src/components/Job/index.js
+++ src/components/Job/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 function Job(props) {
-  return <div />;
+  const history = useHistory();
+
+  function showDetail(e) {
+    e.preventDefault();
+
+    history.push(`/detail/${props.id}`);
+  }
+
+  return (
+    <Container onClick={showDetail}>
+      <ul>
+        <img src={props.company_logo} alt="img" />
+        <div className="content">
+          <h1>{props.title}</h1>
+          <span>{props.company_name}</span>
+          <small>{props.type}</small>
+        </div>
+        <span>{props.created_at}</span>
+      </ul>
+    </Container>
+  )
 }
 
 export default Job;
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 
 import Header from '../../components/Header';
+import Job from '../../components/Job';
 
 import api from '../../services/api';
 
 import { Container, Input } from './styles';
 
 function Main() {
-
+  const [value, setValue] = useState('');
   const [jobs, setJobs] = useState([]);
 
   useEffect(() => {
@@ -25,10 +26,26 @@ function Main() {
         <label>Job Description</label>
         <input
           placeholder="Filter by title, benefits, companies, expertise"
+          defaultValue={value}
+          onChange={e => setValue(e.target.value)}
         >
         </input>
       </Input>
-
+      <div>
+        {jobs
+          .map((job) => (
+            <Job
+              key={job.id + job.title}
+              id={job.id}
+              company_logo={job.company_logo}
+              title={job.title}
+              type={job.type}
+              created_at={job.created_at.parse}
+              company_name={job.company_name}
+            />
+          ))
+        }
+      </div>
     </Container >
   );
 }
